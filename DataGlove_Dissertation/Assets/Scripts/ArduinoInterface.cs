f553a831faf6366e634c328d7020a9bdebe1f253
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-01-14 23:15:48 +0000

    Added two classes for interfacing with the Arduino.

---
+++ DataGlove_Dissertation/Assets/Scripts/ArduinoInterface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO.Ports;
+
+using UnityEngine;
+
+public class ArduinoInterface {
+    private const int _valLen = 5;
+
+    private SerialPort _port;
+    private bool _isValid = false;
+    private float[] _values;
+
+    public ArduinoInterface(string portName) {
+        _port = new SerialPort(portName, 9600);
+
+        try { _port.Open(); }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+            return;
+        }
+        
+        _values = new float[_valLen];
+
+        _isValid = true;
+    }
+
+    public void Update() {
+        if (_isValid) {
+            string[] rawInput = _port.ReadLine().Split(',');
+            int length = rawInput.Length < _valLen ? rawInput.Length : _valLen;
+
+            for (int i = 0; i < length; i++) {
+                if (float.TryParse(rawInput[i], out _values[i])) {
+                    if (_values[i] < 0.0f)
+                        _values[i] = 0.0f;
+                }
+            }
+        }
+    }
+
+    public float GetValue(int index) {
+        if (index < _valLen)
+            return _values[index];
+        else return -1.0f;
+    }
+}
---
+++ DataGlove_Dissertation/Assets/Scripts/DataGloveController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataGloveController : MonoBehaviour {
+    public string portName = "";
+
+    private ArduinoInterface _interface = null;
+
+    void Awake()
+    {
+        if (portName != "")
+            _interface = new ArduinoInterface(portName);
+    }
+
+    void Update()
+    {
+        if (_interface != null)
+            _interface.Update();
+    }
+}
