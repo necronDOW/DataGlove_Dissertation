38f13741acbbac8ff12fff43185f466cbb63acf8
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-02-05 23:33:06 +0000

    Added base code for configuring glove.

--- DataGlove_Dissertation/Assets/Scripts/ArduinoInterface.cs
+++ DataGlove_Dissertation/Assets/Scripts/ArduinoInterface.cs
@@ -3,17 +3,25 @@ using System.IO.Ports;
 
 using UnityEngine;
 
-public class ArduinoInterface {
+public class ArduinoInterface
+{
     private const int _valLen = 5;
 
     private SerialPort _port;
     private bool _isValid = false;
+    private bool _isReady = false;
     private float[] _values;
+    private float _straightR = -1.0f;
+    private float _bendR = -1.0f;
 
-    public ArduinoInterface(string portName) {
+    public ArduinoInterface(string portName)
+    {
         _port = new SerialPort(portName, 9600);
 
-        try { _port.Open(); }
+        try
+        {
+            _port.Open();
+        }
         catch (Exception e)
         {
             Debug.LogError(e.Message);
@@ -21,27 +29,83 @@ public class ArduinoInterface {
         }
         
         _values = new float[_valLen];
-
         _isValid = true;
     }
 
-    public void Update() {
-        if (_isValid) {
-            string[] rawInput = _port.ReadLine().Split(',');
-            int length = rawInput.Length < _valLen ? rawInput.Length : _valLen;
-
-            for (int i = 0; i < length; i++) {
-                if (float.TryParse(rawInput[i], out _values[i])) {
-                    if (_values[i] < 0.0f)
-                        _values[i] = 0.0f;
-                }
-            }
+    public void Update()
+    {
+        for (int i = 0; i < ReadRawSerial(); i++)
+        {
+            MapToAngle(ref _values[i]);
+            Debug.Log(_values[i]);
         }
     }
 
-    public float GetValue(int index) {
+    public bool Config()
+    { 
+        ReadRawSerial();
+        SetStraightR(11622.0f);
+        SetBendR(31278.0f);
+
+        return _isReady;
+    }
+
+    private int ReadRawSerial()
+    {
+        if (_isValid && _isReady)
+        {
+            string[] rawInput = _port.ReadLine().Split(',');
+            int length = (rawInput.Length - 1) < _valLen ? (rawInput.Length - 1) : _valLen;
+
+            for (int i = 0; i < length; i++)
+            {
+                float.TryParse(rawInput[i], out _values[i]);
+            }
+
+            return length;
+        }
+
+        return -1;
+    }
+
+    private void MapToAngle(ref float value)
+    {
+        value = (90.0f * ((value - _straightR) / (_bendR - _straightR)));
+    }
+
+    private bool SetStraightR(float value)
+    {
+        _straightR = value;
+
+        if (_bendR >= 0.0f)
+        {
+            _isReady = true;
+        }
+
+        return _isReady;
+    }
+
+    private bool SetBendR(float value)
+    {
+        _bendR = value;
+
+        if (_straightR >= 0.0f)
+        {
+            _isReady = true;
+        }
+
+        return _isReady;
+    }
+
+    public float GetValue(int index)
+    {
         if (index < _valLen)
+        {
             return _values[index];
-        else return -1.0f;
+        }
+        else
+        {
+            return -1.0f;
+        }
     }
 }
--- DataGlove_Dissertation/Assets/Scripts/DataGloveController.cs
+++ DataGlove_Dissertation/Assets/Scripts/DataGloveController.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataGloveController : MonoBehaviour {
+public class DataGloveController : MonoBehaviour
+{
     public string portName = "";
+    private enum Mode
+    {
+        Default,
+        Config
+    }
+    private Mode _mode = Mode.Config;
 
     private ArduinoInterface _interface = null;
 
     void Awake()
     {
         if (portName != "")
+        {
             _interface = new ArduinoInterface(portName);
+        }
     }
 
     void Update()
     {
         if (_interface != null)
-            _interface.Update();
+        {
+            switch (_mode)
+            {
+                case Mode.Default:
+                    _interface.Update();
+                    break;
+                case Mode.Config:
+                    if (_interface.Config())
+                    {
+                        _mode = Mode.Default;
+                    }
+                    break;
+            }
+        }
     }
 }
