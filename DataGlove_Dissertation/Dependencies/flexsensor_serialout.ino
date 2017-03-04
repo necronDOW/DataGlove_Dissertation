abfa8d3b6ce927b0f49680e0830ebf321192e0d0
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-03-04 22:03:45 +0000

    Dependencies added.
    Added arduino .ino file for the flex sensor readings.

---
+++ DataGlove_Dissertation/Dependencies/flexsensor_serialout.ino
@@ -0,0 +1,22 @@
+const int sensorCount = 5;
+const float vcc = 4.98;
+const float rDiv = 20000.0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int i = 0; i < sensorCount; i++) {
+    Serial.print(String(GetR(i)) + ",");
+  }
+  
+  Serial.println();
+}
+
+float GetR(int pinIndex) {
+  int flexADC = analogRead(A0 + pinIndex);
+  float flexV = flexADC * vcc / 1023.0;
+  return rDiv * (vcc / flexV - 1.0);
+}
+
